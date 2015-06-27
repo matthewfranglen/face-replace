7181ba520ca40d34e73a28db5c49465e4fe62d31
Matthew Franglen <matthew@franglen.org>
2015-06-27 19:46:44 +0100

    Add masking and starting on color calculations

--- js/main.js
+++ js/main.js
@@ -10,13 +10,89 @@ angular.module('faceReplaceApp', [])
         tracker.on('track', callback);
     };
 
+    var calculateCrop = function (targetBox) {
+        return {
+            'width': targetBox.width,
+            'height': targetBox.height,
+            'top': targetBox.y,
+            'left': targetBox.x
+        };
+    };
+
+    var calculateImage = function (sourceImage, sourceBox, targetBox) {
+        var widthRatio = targetBox.width / sourceBox.width;
+        var heightRatio = targetBox.height / sourceBox.height;
+
+        return {
+            'width': sourceImage.width() * widthRatio,
+            'height': sourceImage.height() * heightRatio,
+            'top': -(sourceBox.y * heightRatio),
+            'left': -(sourceBox.x * widthRatio)
+        };
+    };
+
+    var getImageCanvas = function (image, x, y, width, height) {
+        var canvas;
+
+        canvas = angular.element('<canvas/>')[0];
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(image[0], -x, -y, width + x, height + y);
+
+        return canvas.getContext('2d');
+    };
+
+    var generateAlphaMap = function (canvas, width, height) {
+        var imageData, alphaMap, i;
+
+        imageData = canvas.getImageData(0, 0, width, height).data;
+        alphaMap = [];
+        alphaMap.length = width * height;
+
+        for (i = 0; i < alphaMap.length; i++) {
+            alphaMap[i] = imageData[(i * 4) + 3];
+        }
+
+        return alphaMap;
+    };
+
+    var mostCommonColor = function (canvas, width, height, alphaMap) {
+        var imageData, average, count, i, j, alpha;
+
+        imageData = canvas.getImageData(0, 0, width, height).data;
+
+        average = [0, 0, 0];
+        count = 0;
+
+        for (i = 0; i < imageData.length; i += 4) {
+            alpha = alphaMap[i / 4];
+            count += alpha;
+
+            for (j = 0; j < average.length; j++) {
+                average[j] += imageData[i + j] * alpha;
+            }
+        }
+
+        for (i = 0; i < average.length; i++) {
+            average[i] /= count;
+        }
+
+        return average;
+    };
+
     var replaceFaces = function () {
-        var replaceFace = function (face) {
+        var sourceImage = angular.element('#source');
+        var sourceBox = faceReplace.sources[0];
+        var targetImage = angular.element('#target');
+
+        var replaceFace = function (targetBox) {
+            var canvas = getImageCanvas(targetImage, targetBox.x, targetBox.y, targetBox.width, targetBox.height);
+            var commonColor = mostCommonColor(canvas, targetBox.width, targetBox.height, faceReplace.alphaMap);
+            console.log(commonColor);
+
             faceReplace.targets.push({
-                'top': face.y,
-                'left': face.x,
-                'width': face.width,
-                'height': face.height
+                'crop': calculateCrop(targetBox),
+                'img': calculateImage(sourceImage, sourceBox, targetBox)
             });
         };
 
@@ -31,14 +107,25 @@ angular.module('faceReplaceApp', [])
         });
     };
 
-    faceReplace.sourceImage = "static/glenn.jpg";
     faceReplace.sources = [];
     faceReplace.targets = [];
 
     trackFaces('#source', function(trackEvent) {
-        trackEvent.data.forEach(function (data) { faceReplace.sources.push(data); });
+        trackEvent.data.forEach(function (data) {
+            faceReplace.sources.push(data);
+        });
+
+        var maskedImage = angular.element('<img src="static/masked-glenn.png" />');
+        maskedImage.one('load', function () {
+            var sourceBox = faceReplace.sources[0];
+            var canvas = getImageCanvas(maskedImage, sourceBox.x, sourceBox.y, sourceBox.width, sourceBox.height);
+            faceReplace.alphaMap = generateAlphaMap(canvas, sourceBox.width, sourceBox.height);
+            faceReplace.mostCommonColor = mostCommonColor(canvas, sourceBox.width, sourceBox.height, faceReplace.alphaMap);
+
+            console.log(faceReplace.alphaMap);
+            console.log(faceReplace.mostCommonColor);
+        });
+
         replaceFaces();
     });
-
-    console.log('Configured');
 }]);
