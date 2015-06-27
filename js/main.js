7b2b61be4570a794075765b583347f581f2e53fc
Matthew Franglen <matthew@franglen.org>
2015-06-27 12:53:37 +0100

    Next stage

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 /*jslint browser: true*/
 /*global angular, tracking*/
 angular.module('faceReplaceApp', [])
-       .controller('FaceReplaceController', ['$scope', function($scope) {
-
+.controller('FaceReplaceController', ['$scope', function($scope) {
     var faceReplace = this;
-    var video = document.getElementById('video');
-    var tracker = new tracking.ObjectTracker('face');
 
-    faceReplace.faces = [];
-
-    tracking.track(video, tracker);
-
-    var clearFaces = function() {
-        faceReplace.faces.length = 0;
+    var trackFaces = function (target, callback) {
+        var tracker = new tracking.ObjectTracker('face');
+        tracking.track(target, tracker);
+        tracker.on('track', callback);
     };
-    var addFace = function(face) {
-        faceReplace.faces.push({
-            'top': face.y,
-            'left': face.x,
-            'width': face.width,
-            'height': face.height
+
+    var replaceFaces = function () {
+        var replaceFace = function (face) {
+            faceReplace.targets.push({
+                'top': face.y,
+                'left': face.x,
+                'width': face.width,
+                'height': face.height
+            });
+        };
+
+        trackFaces('#target', function (trackEvent) {
+            if (faceReplace.sources.length === 0) {
+                console.log("No faces detected in source image...");
+                return;
+            }
+
+            trackEvent.data.forEach(replaceFace);
+            $scope.$apply();
         });
-        console.log(faceReplace.faces);
-    };
-    var handleTrackEvent = function(trackEvent) {
-        console.log(trackEvent);
-        clearFaces();
-        trackEvent.data.forEach(addFace);
-        $scope.$apply();
     };
 
-    tracker.on('track', handleTrackEvent);
+    faceReplace.sourceImage = "static/glenn.jpg";
+    faceReplace.sources = [];
+    faceReplace.targets = [];
+
+    trackFaces('#source', function(trackEvent) {
+        trackEvent.data.forEach(function (data) { faceReplace.sources.push(data); });
+        replaceFaces();
+    });
 
     console.log('Configured');
 }]);
